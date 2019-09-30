429d2616779a0aedcaa3076b10b4665a403f3acc
GuilhermeM06 <53957388+GuilhermeM06@users.noreply.github.com>
2019-09-30 19:05:24 -0300

    Atualização de tabelas.

--- trabalho LMS.sql
+++ trabalho LMS.sql
@@ -5,5 +5,30 @@ Senha varchar(10) not null,
 DtExpiracao DATE CONSTRAINT dfDataVenda DEFAULT ('1900-01-01'),
 CONSTRAINT pkId primary key(ID),
 constraint ukLogin unique(login));
- 
- select * from Usuario
+
+create table Coordenador
+(id tinyint identity,
+id_usuario tinyint,
+Nome varchar(50)NOT NULL,
+Email varchar(50) NOT NULL, 
+Celular tinyint NOT NULL,
+constraint pkIdCoordenador primary key(id),
+constraint ukEmail unique(Email),
+constraint ukCelular unique(Celular),
+constraint fkIdUsuario foreign key(id_usuario)
+references Usuario(ID)); 
+ select * from Coordenador
+
+create table Aluno
+(ID tinyint identity,
+id_usuario tinyint,
+Nome varchar(50)NOT NULL,
+Email varchar(50) NOT NULL, 
+Celular tinyint NOT NULL,
+RA char(7) NOT NULL,
+Foto varchar(200), 
+constraint pkIdAluno primary key(ID),
+constraint ukEmailAluno unique(Email),
+constraint ukCelularAluno unique(Celular),
+constraint fkIdUsuarioAluno foreign key(id_usuario)
+references Usuario(ID)); 
