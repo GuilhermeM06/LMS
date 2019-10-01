3822858f4c2fbd413e3cacbd6753a08820698d1b
Guilherme Alves dos Santos <1900785@SALAS.AULAS>
2019-10-01 19:00:03 -0300

    Adicionando tabela DisciplinasOfertadas

--- LMS.sql
+++ LMS.sql
@@ -6,8 +6,8 @@ Hadnan Basilio, 1901020
 Victor Sanches Barbosa, 
 */
 
-create TABLE Usuario
-(ID tinyint identity(1,1),
+CREATE TABLE Usuario
+(ID TINYINT IDENTITY(1,1),
 Login varchar(10) not null,
 Senha varchar(10) not null,
 DtExpiracao DATE CONSTRAINT dfDataVenda DEFAULT ('1900-01-01'),
@@ -15,7 +15,7 @@ CONSTRAINT pkId primary key(ID),
 constraint ukLogin unique(login));
 
 create table Coordenador
-(id tinyint identity,
+(ID tinyint identity,
 id_usuario tinyint,
 Nome varchar(50)NOT NULL,
 Email varchar(50) NOT NULL, 
@@ -34,7 +34,7 @@ Nome varchar(50)NOT NULL,
 Email varchar(50) NOT NULL, 
 Celular tinyint NOT NULL,
 RA char(7) NOT NULL,
-Foto varchar(200), 
+Foto varchar(100), 
 constraint pkIdAluno primary key(ID),
 constraint ukEmailAluno unique(Email),
 constraint ukCelularAluno unique(Celular),
@@ -58,14 +58,14 @@ ID tinyint identity,
 Nome varchar(50) NOT NULL,
 DataDisciplina date constraint dfDataDisciplina default getdate(),
 StatusDisciplina varchar(7) NOT NULL constraint dfStatusDisciplina default 'Aberta',
-PlanoDeEnsino varchar(200),
+PlanoDeEnsino varchar(100),
 CargaHoraria tinyint NOT NULL,
 Competencias varchar(200),
 Habilidades varchar(200),
-Ementa varchar(200),
-ConteudoProgramatico varchar(200),
-BibliografiaBasica varchar(200),
-BibliografiaComplementar varchar(200),
+Ementa varchar(100),
+ConteudoProgramatico varchar(100),
+BibliografiaBasica varchar(100),
+BibliografiaComplementar varchar(100),
 PercentualPratico tinyint,
 PercentualTeorico tinyint,
 IdCoordenador tinyint,
@@ -81,5 +81,30 @@ constraint ckPercentualTeorico check (PercentualTeorico >= 0 and PercentualTeori
 create table Curso(
 ID tinyint identity,
 Nome varchar(50) NOT NULL,
-constraint ukNomeCurso unique (Nome)
+constraint ukNomeCurso unique (Nome),
+CONSTRAINT pkIdCurso PRIMARY KEY (ID)
 ); 
+
+create table DisciplinaOfertada
+(ID tinyint identity,
+IdCoordenador tinyint NOT NULL,
+DtInicioMatricula date, 
+DtFimMatricula date,
+IdDisciplina tinyint NOT NULL,
+IdCurso tinyint NOT NULL,
+Ano SMALLINT NOT NULl, 
+Semestre tinyint NOT NULL,
+Turma char(1) NOT NULL,
+IdProfessor tinyint,
+Metodologia varchar (100),
+Recursos varchar(100),
+CriterioAvaliacao varchar (100),
+PlanoDeAulas varchar (100)
+CONSTRAINT pkIdDisciplinaOfertada PRIMARY KEY (ID),
+CONSTRAINT fkIdCoordenador FOREIGN KEY (IdCoordenador) REFERENCES Coordenador(ID),
+CONSTRAINT fkIdDisciplina FOREIGN KEY (idDisciplina) REFERENCES Disciplina (ID),
+CONSTRAINT fkIdCurso FOREIGN KEY (IdCurso) REFERENCES Curso (ID),
+CONSTRAINT idProfessor FOREIGN KEY (IdProfessor) REFERENCES Professor (ID),
+CONSTRAINT ckAno CHECK (Ano Between 1900 and 2100),
+CONSTRAINT ckSemestre CHECK (Semestre = 1 or Semestre = 2), 
+CONSTRAINT cKTurma CHECK (Turma like '[A-Z]'));
